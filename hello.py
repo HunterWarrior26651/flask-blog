f8863058ec0b415002fdacdbfb0dbbbd3f649b41
HunterWarrior26651 <wookie99@tpg.com.au>
2022-09-03 20:13:33 +0800

    1254 file commit

--- hello.py
+++ hello.py
@@ -36,3 +36,10 @@ def index():
 def user(name):
     return render_template("user.html", user_name=name)
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template("500.html"), 404
