10b18195c22784fba7159d2826d9aaeb7962236b
HunterWarrior26651 <wookie99@tpg.com.au>
2022-09-14 19:07:35 +0800

    hashing passwords with Werkzeug

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-# 12
+# 13
 # export FLASK_ENV=development
 # export FLASK_APP=hello.py
 
@@ -10,6 +10,8 @@ from wtforms.validators import DataRequired
 from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from werkzeug.security import generate_password_hash, check_password_hash
+
 
 # Create a Flask Instance
 app = Flask(__name__)
@@ -31,6 +33,22 @@ class Users(db.Model):
     email = db.Column(db.String(120), nullable=False, unique=True)
     favorite_color = db.Column(db.String(120))
     date_added = db.Column(db.DateTime, default=datetime.utcnow)
+    # Do some password stuff!
+    password_hash = db.Column(db.String(128))
+
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute!')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+
+
 
     # Create A String
     def __repr__(self):
