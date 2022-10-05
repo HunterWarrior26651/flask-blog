fef33a60dbbc29befc594372a0c014ce888a0333
HunterWarrior26651 <wookie99@tpg.com.au>
2022-10-05 18:50:55 +0800

    set default profile pic

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-# 30
+# 35
 # export FLASK_ENV=development
 # export FLASK_APP=hello.py
 
@@ -43,6 +43,16 @@ def base():
     form = SearchForm()
     return dict(form=form)
 
+# Create Admin Page
+@app.route('/admin')
+@login_required
+def admin():
+    id = current_user.id
+    if id == 11: # 11 is the id of the admin person
+        return render_template("admin.html")
+    else:
+        flash("Sorry you must be the admin to access the Admin Page...")
+        return redirect(url_for('dashboard'))
 
 # Create Search Function
 @app.route('/search', methods=["POST"])
